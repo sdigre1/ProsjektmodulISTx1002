897695e26bb48d29e54d1b9f80cca56fcc90ea3f
sdigre1/Sigurd <you@example.com>
2024-10-24 15:22:09 +0200

    Endret på mappestruktur

---
+++ Sigurd/v.1.2.0.py
@@ -0,0 +1,11 @@
+# For å jobbe med første del av notatboken trenger vi disse bibliotekene
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.patches import Rectangle
+
+# Først setter vi lengder og vinkler:
+theta1 = 30     # Vinkel mellom positiv x-akse og "overarm" i grader
+l1 = 60        # Lengde til overarm (f. eks. i meter)
+theta2 = 30   # Vinkel mellom overarm og underarm i grader
+l2 = 50        # Lengde til underarm (bruk samme lengde-enhet som for l1).
